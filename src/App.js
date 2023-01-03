a57c9ebb965d35301d46eb56153b07aa42465b90
Emily Gong <emily.gong@vivaleisure.com.au>
2023-01-03 12:27:36 +1000

    feat: international mobile input

--- src/App.js
+++ src/App.js
@@ -1,22 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import "react-phone-number-input/style.css";
+import { useState } from "react";
+import PhoneInput, {
+  formatPhoneNumber,
+  formatPhoneNumberIntl,
+  isValidPhoneNumber,
+  isPossiblePhoneNumber,
+} from "react-phone-number-input";
+const getMember = { mobile: "+61413549397" };
 function App() {
+  const [value, setValue] = useState(getMember.mobile);
+  console.log(value);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <div style={{ width: "200px" }}>
+          <PhoneInput
+            initialValueFormat="national"
+            placeholder="Enter phone number"
+            defaultCountry="AU"
+            value={value}
+            onChange={setValue}
+          />
+        </div>
+        <p style={{ color: "red" }}>
+          {value && isValidPhoneNumber(value)
+            ? ""
+            : "Please input a correct mobile number"}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>
+          Is possible:{" "}
+          {value && isPossiblePhoneNumber(value) ? "true" : "false"}
+        </p>
+        <p>Is valid: {value && isValidPhoneNumber(value) ? "true" : "false"}</p>
+        <p>National: {value && formatPhoneNumber(value)}</p>
+        <p>International: {value && formatPhoneNumberIntl(value)}</p>
       </header>
     </div>
   );
