2c8fdb6df829c1ab87fd6fd9f9b90489f36e3d6a
Emily Gong <emily.gong@vivaleisure.com.au>
2023-01-11 14:46:14 +1000

    feat: validator prac

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import { useState } from "react";
+import isMobilePhone from "validator/lib/isMobilePhone";
+import isEmail from "validator/lib/isEmail";
 import PhoneInput from "react-phone-input-2";
 import "react-phone-input-2/lib/style.css";
 
 const getMember = { mobile: "+61413549397" };
 function App() {
   const [phone, setPhone] = useState(getMember.mobile);
-  const [isPhoneValid, setPhoneValid] = useState(false);
+  // const [isPhoneValid, setPhoneValid] = useState(false);
   console.log(phone);
-  console.log(isPhoneValid);
+  console.log(isMobilePhone(phone, "zh-CN"));
+  console.log(isEmail("emily.gong@gmail"));
   return (
     <div className="App">
       <header className="App-header">
@@ -17,19 +20,19 @@ function App() {
             value={phone}
             onChange={(value, country, formattedValue) => {
               setPhone(value);
-              console.log(value, country, formattedValue);
-              if (country.format?.length === formattedValue?.length) {
-                setPhoneValid(true);
-              } else {
-                setPhoneValid(false);
-              }
+              // console.log(value, country, formattedValue);
+              // if (country.format?.length === formattedValue?.length) {
+              //   setPhoneValid(true);
+              // } else {
+              //   setPhoneValid(false);
+              // }
             }}
             enableSearch={true}
             specialLabel="hello"
           />
-          {!isPhoneValid && (
+          {/* {!isPhoneValid && (
             <p style={{ color: "red" }}>Please input valid number!</p>
-          )}
+          )} */}
         </div>
       </header>
     </div>
